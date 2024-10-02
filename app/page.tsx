b940d95b14c9e73c484c8447ff477425cd11e9db
Centrric <hello@centrric.com>
2024-10-02 12:37:35 +0400

    setting up

--- app/page.tsx
+++ app/page.tsx
@@ -1,142 +1,70 @@
-"use client";
-import { useRef, useEffect, useState, useCallback, useMemo } from "react";
+import { ParticlesLoader } from "@/components/ParticleLoader";
+import { ModeToggle } from "@/components/ui/themeToggle";
+import { Button } from "@/components/ui/button";
+import test3 from "@/assets/test3.jpg"
+import Image from "next/image";
 
-const colors = ["#63dcec", "#11caeb", "#3309ee", "#09e7f7", "#8823fc"];
-const text = "Salman";
-
-// const  particles = [] as any
-
-export default function Home() {
-  const canvasRef = useRef<HTMLCanvasElement>(null) as any;
-
-  const particlesRef = useRef() as any;
-  particlesRef.current = [];
-
-
-  // const [mouses, setmouse] = useState({ x: 0, y: 0 })
-  const mouse = useMemo(() => ({ x: 0, y: 0 }), []);
-  let radius = 1;
-
-
-
-  const initScene = useCallback(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    const ww = (canvas.width = window.innerWidth);
-    const wh = (canvas.height = window.innerHeight);
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = `bold ${ww / 10}px sans-serif`;
-    ctx.textAlign = "center";
-    ctx.fillText(text, ww / 2, wh / 2);
-
-    const data = ctx.getImageData(0, 0, ww, wh).data;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.globalCompositeOperation = "screen";
-
-    for (let i = 0; i < ww; i += Math.round(ww / 150)) {
-      for (let j = 0; j < wh; j += Math.round(ww / 150)) {
-        if (data[(i + j * ww) * 4 + 3] > 150) {
-          particlesRef.current.push(new Particle(i, j, ww, wh, colors));
-        }
-      }
-    }
-  }, []);
-
-  // const onMouseMove = (e: { clientX: number; clientY: number; }) => {
-  //   setmouse({ x: e.clientX, y: e.clientY });
-  // };
-
-
-
-  const render = useCallback(() => {
-
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    particlesRef.current.forEach(
-      (particle: { render: (arg0: any, arg1: { x: number; y: number }, arg2: number) => any }) =>
-        particle.render(ctx, mouse, radius)
-    );
-    requestAnimationFrame(render);
-  },[mouse, radius]);
-
-  useEffect(() => {
-    initScene();
-    render();
-    // window.addEventListener("mousemove", onMouseMove);
-    return () => {
-      // window.removeEventListener("mousemove", onMouseMove);
-    };
-  }, [initScene, render]);
-
+export default function Page() {
   return (
-    <main className="bg-black">
-      <canvas ref={canvasRef} />
+    <main className="flex   justify-center">
+      <ParticlesLoader />
+
+      {/* <ModeToggle />
+
+      <Button>
+        This is a test button
+      </Button> */}
+
+      <div className="max-w-[700px]">
+        <div>
+          <div>
+            <h2 className="text-white text-4xl py-4">Hi, I,m Amal Antony</h2>
+            <h4 className="my-2">Software Developer focused in Frontend. Self-taught and always eager to learn new technologies.</h4>
+          </div>
+
+          <div>
+            <Image src={""} alt="" />
+          </div>
+        </div>
+
+        <div>
+          <h3 className="my-4 text-xl font-medium">About Me</h3>
+          <h4>
+            {` I'm a passionate and dedicated software developer, constantly seeking to enhance my skills and tackle new technological challenges. Started my career as a Mobile Developer focused in Android and now I'm a Frontend Developer focused in creating responsive and modern web applications using React.
+`}
+          </h4>
+        </div>
+        <div>
+    
+          <div>
+          <h3 className="my-4 text-xl font-medium">
+          Professional Experience
+
+          </h3>
+          <div className="flex flex-col">
+
+<div className="border flex border-gray-50 bg-gray-50 opacity-20 p-5 gap-5 rounded-xl">
+<Image src={test3} width={50} height={50} alt="" className="rounded-full" />
+
+<div className="flex-1 border">
+
+<h2 className="">
+  Centrric Innnovations
+</h2>
+<h4>
+
+</h4>
+</div>
+
+</div>
+<div>
+
+</div>
+
+          </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
-}
-
-class Particle {
-  x: number;
-  y: number;
-  dest: { x: number; y: number };
-  r: number;
-  vx: number;
-  vy: number;
-  accX: number;
-  accY: number;
-  friction: number;
-  color: any;
-  constructor(x: number, y: number, ww: number, wh: number, colors: string | any[]) {
-    this.x = Math.random() * ww;
-    this.y = Math.random() * wh;
-    this.dest = { x, y };
-    this.r = Math.random() * 5 + 2;
-    this.vx = (Math.random() - 0.5) * 20;
-    this.vy = (Math.random() - 0.5) * 20;
-    this.accX = 0;
-    this.accY = 0;
-    this.friction = Math.random() * 0.05 + 0.94;
-    this.color = colors[Math.floor(Math.random() * colors.length)];
-  }
-
-  render(
-    ctx: {
-      shadowColor: any;
-      shadowBlur: number;
-      fillStyle: any;
-      beginPath: () => void;
-      arc: (arg0: any, arg1: any, arg2: any, arg3: number, arg4: boolean) => void;
-      fill: () => void;
-    },
-    mouse: { x: number; y: number },
-    radius: number
-  ) {
-    this.accX = (this.dest.x - this.x) / 1000;
-    this.accY = (this.dest.y - this.y) / 1000;
-    this.vx += this.accX;
-    this.vy += this.accY;
-    this.vx *= this.friction;
-    this.vy *= this.friction;
-    this.x += this.vx;
-    this.y += this.vy;
-
-    ctx.fillStyle = this.color;
-    ctx.shadowColor = this.color;
-    ctx.shadowBlur = 20;
-    ctx.beginPath();
-    ctx.arc(this.x, this.y, this.r, Math.PI * 2, false);
-    ctx.fill();
-
-    const a = this.x - mouse.x;
-    const b = this.y - mouse.y;
-    const distance = Math.sqrt(a * a + b * b);
-    if (distance < radius * 70) {
-      this.accX = (this.x - mouse.x) / 100;
-      this.accY = (this.y - mouse.y) / 100;
-      this.vx += this.accX;
-      this.vy += this.accY;
-    }
-  }
 }
---
+++ components/ParticleLoader.tsx
@@ -0,0 +1,132 @@
+'use client'
+
+import { useEffect, useMemo, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import {
+  type Container,
+  type ISourceOptions,
+  MoveDirection,
+  OutMode,
+} from "@tsparticles/engine";
+// import { loadAll } from "@tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import { useTheme } from "next-themes";
+// import { loadBasic } from "@tsparticles/basic"; // if you are going to use `loadBasic`, install the "@tsparticles/basic" package too.
+
+export const ParticlesLoader = () => {
+  const [init, setInit] = useState(false);
+  const { setTheme, theme } = useTheme()
+
+  // this should be run only once per application lifetime
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      // you can initiate the tsParticles instance (engine) here, adding custom shapes or presets
+      // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+      // starting from v2 you can add only the features you need reducing the bundle size
+      //await loadAll(engine);
+      //await loadFull(engine);
+      await loadSlim(engine);
+      //await loadBasic(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = async (container?: Container): Promise<void> => {
+    console.log(container);
+  };
+
+  const options: ISourceOptions = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: theme === "dark" ? "black" : "#e0e0e0",  
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "push",
+          },
+          onHover: {
+            enable: true,
+            mode: "connect",
+          },
+        },
+        modes: {
+          push: {
+            quantity: 8,
+          },
+          repulse: {
+            distance: 120,
+            duration: 0.4,
+          },
+
+          bubble: {
+            size: 18,
+            distance: 100,
+            duration: 0.5
+          }
+
+          //           bubble.distance	number	100	
+          // bubble.size	number	40	
+          // bubble.duration
+        },
+      },
+      particles: {
+        color: {
+          value: theme === "dark" ? "#ffffff" : "#000000",
+        },
+        links: {
+          color: theme === "dark" ? "#ffffff" : "#000000",
+          distance: 150,
+          enable: true,
+          opacity: 0.2,
+          width: 1,
+        },
+        move: {
+          direction: MoveDirection.none,
+          enable: true,
+          outModes: {
+            default: OutMode.out,
+          },
+          random: false,
+          speed: 2,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 180,
+        },
+        opacity: {
+          value: 0.5,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    [],
+  );
+
+  if (init) {
+    return (
+      <Particles
+        id="tsparticles"
+        particlesLoaded={particlesLoaded}
+        options={options}
+      />
+    );
+  }
+
+  return <></>;
+};
